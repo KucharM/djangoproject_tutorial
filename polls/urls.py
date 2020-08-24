369575e8869cd8bc9f5be3f4d89fe9a7f9a21271
mm.kucharski@gmail.com <mm.kucharski@gmail.com>
2020-08-24 13:25:50 +0200

    create urls in polls

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import index
+from .views import index, result,vote, detail
 
 
 urlpatterns = [
     path('', index, name='index'),
+    path('<int:question_id>/', detail, name='detail'),
+    path('<int:question_id>/results/', result, name='result'),
+    path('<int:question_id>/vote/', vote, name='vote'),
 ]
