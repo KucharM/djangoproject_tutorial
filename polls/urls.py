c741a5b8afe285444cc326a604751a997296db82
mm.kucharski@gmail.com <mm.kucharski@gmail.com>
2020-08-24 14:08:32 +0200

    added app_name to polls urls and changed url in index template

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .views import index, result,vote, detail
 
 
+app_name = 'polls'
+
 urlpatterns = [
     path('', index, name='index'),
     path('<int:question_id>/', detail, name='detail'),
