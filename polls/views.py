31516b4a944531cabefc4fb9014101b5cf92ad03
mm.kucharski@gmail.com <mm.kucharski@gmail.com>
2020-08-24 13:25:12 +0200

    create simple views for polls

--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,15 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('Hello world, welcome to polls app!')
+
+
+def detail(request, question_id):
+    return HttpResponse(f'you are looking for question {question_id}')
+
+
+def result(request, question_id):
+    return HttpResponse(f'you are looking for result question {question_id}')
+
+
+def vote(request, question_id):
+    return HttpResponse(f'you are voting for question {question_id}')
